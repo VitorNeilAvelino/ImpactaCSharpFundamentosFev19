87ad80298c63e0e8ade6d08bf604bc8ffb332ee9
Vitor Neil Avelino <avelino.vitor@gmail.com>
2019-02-15 22:32:27 -0200

    Frete

--- CSharpFundamentos.Capitulo03.Troco/TrocoForm.cs
+++ CSharpFundamentos.Capitulo03.Troco/TrocoForm.cs
@@ -27,6 +27,35 @@ namespace CSharpFundamentos.Capitulo03.Troco
             decimal troco = valorPago - valorCompra;
 
             trocoTextBox.Text = /*"R$ " + */ troco.ToString("C");
+
+            //ToDo: trocar por estrutura de repetição.
+            var moedas1 = (int)troco;
+            //troco = troco - moedas1;
+            //troco -= moedas1;
+            //troco = troco % 1;
+            troco %= 1;
+
+            var moedas050 = (int)(troco / 0.5m);
+            troco %= 0.5m;
+
+            var moedas025 = (int)(troco / 0.25m);
+            troco %= 0.25m;
+
+            var moedas010 = (int)(troco / 0.1m);
+            troco %= 0.1m;
+
+            var moedas005 = (int)(troco / 0.05m);
+            troco %= 0.05m;
+
+            var moedas001 = (int)(troco / 0.01m);
+            troco %= 0.01m;
+
+            moedasListView.Items[0].Text = moedas1.ToString();
+            moedasListView.Items[1].Text = moedas050.ToString();
+            moedasListView.Items[2].Text = moedas025.ToString();
+            moedasListView.Items[3].Text = moedas010.ToString();
+            moedasListView.Items[4].Text = moedas005.ToString();
+            moedasListView.Items[5].Text = moedas001.ToString();
         }
     }
 }
---
+++ CSharpFundamentos.Capitulo04.Frete/FreteForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpFundamentos.Capitulo04.Frete
+{
+    public partial class FreteForm : Form
+    {
+        public FreteForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calcularButton_Click(object sender, EventArgs e)
+        {
+            var erros = ValidarFormulario();
+
+            if (erros.Count == 0)
+            {
+                Calcular();
+            }
+            else
+            {
+                MessageBox.Show(string.Join(Environment.NewLine, erros),
+                    "Validação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void Calcular()
+        {
+            var percentual = 0M;
+            var valor = Convert.ToDecimal(valorTextBox.Text);
+
+            switch (ufComboBox.Text.ToUpper())
+            {
+                case "SP":
+                    percentual = 0.2m;
+                    break;
+                //case "SP":
+                //    percentual = 0.2m;
+                //    break;
+                case "ES":
+                case "RJ":
+                    percentual = 0.3m;
+                    break;
+                case "MG":
+                    percentual = 0.35m;
+                    break;
+                case "AM":
+                    percentual = 0.6m;
+                    break;
+                default:
+                    percentual = 0.75m;
+                    break;
+            }
+
+            if (ufComboBox.Text == "SP")
+            {
+                percentual = 0.2m;
+            }
+            else if (ufComboBox.Text == "RJ")
+            {
+                percentual = 0.3m;
+            }
+            else if (ufComboBox.Text == "MG")
+            {
+                percentual = 0.35m;
+            }
+            else
+            {
+                percentual = 0.75m;
+            }
+
+        }
+
+        private List<string> ValidarFormulario()
+        {
+            var erros = new List<string>();
+
+            if (clienteTextBox.Text == "")
+            {
+                erros.Add("O campo Cliente é obrigatório.");
+            }
+
+            if (valorTextBox.Text == string.Empty)
+            {
+                erros.Add("O campo Valor é obrigatório.");
+            }
+            else
+            {
+                decimal valor = 0;
+
+                if (!decimal.TryParse(valorTextBox.Text, out valor))
+                {
+                    erros.Add("O campo Valor está com formato inválido.");
+                }
+            }
+
+            if (ufComboBox.SelectedIndex == -1)
+            {
+                erros.Add("Selecione uma UF.");
+            }
+
+            return erros;
+        }
+    }
+}
